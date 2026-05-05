ee16c473f223f6c54f5f636139f0d2a0834dfb0d
Santiago Narvaez <santiagonar1@gmail.com>
2026-05-05 17:23:58 +0200

    feat: add support for reading scanner progress and magnet field strength

--- app/client.cpp
+++ app/client.cpp
@@ -11,14 +11,26 @@ int main() {
         return static_cast<int>(status);
     }
 
-    const auto value = client.read_attribute_custom_node<int32_t>("mri.scanner.scanprogress");
+    const auto scanner_progress =
+            client.read_attribute_custom_node<int32_t>("mri.scanner.scanprogress");
 
-    if (not value.has_value()) {
-        std::cerr << "Failed to read the value" << std::endl;
+    if (not scanner_progress.has_value()) {
+        std::cerr << "Failed to read the scanner progress" << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::cout << "the value is: " << value.value() << std::endl;
+    std::cout << "The scanner progress is: " << scanner_progress.value() << std::endl;
+
+    const auto field_strength =
+            client.read_attribute_custom_node<float>("mri.scanner.magnet.fieldstrength");
+
+    if (not field_strength.has_value()) {
+        std::cerr << "Failed to read the field strength" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "The field strength is: " << field_strength.value() << std::endl;
+
 
     return EXIT_SUCCESS;
 }
