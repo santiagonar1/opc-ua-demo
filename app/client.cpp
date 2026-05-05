cdffe580cd0f1e6c7acb300b50411b0217294ef7
Santiago Narvaez <santiagonar1@gmail.com>
2026-05-05 13:31:19 +0200

    feat: add read_attribute_custom_node method to client class

--- app/client.cpp
+++ app/client.cpp
@@ -1,25 +1,24 @@
-#include <open62541/client.h>
-#include <open62541/client_highlevel.h>
-#include <stdio.h>
-
 #include <client.hpp>
-#include <variant.hpp>
+
+#include <iostream>
+#include <ostream>
 
 int main() {
-    const auto client = demo::Client{};
+    auto client = demo::Client{};
 
-    auto status = client.connect("opc.tcp://localhost:4840");
-    if (status != UA_STATUSCODE_GOOD) { return static_cast<int>(status); }
-
-    auto value = demo::make_variant(); /* Variants can hold scalar values and arrays of any type */
-    status = UA_Client_readValueAttribute(client.get_client(), UA_NODEID_STRING(1, "the.answer"),
-                                          &value);
-    if (status == UA_STATUSCODE_GOOD &&
-        UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32])) {
-        printf("the value is: %i\n", *(UA_Int32 *) value.data);
+    if (const auto status = client.connect("opc.tcp://localhost:4840");
+        status != UA_STATUSCODE_GOOD) {
+        return static_cast<int>(status);
     }
 
-    /* Clean up */
-    UA_Variant_clear(&value);
-    return status == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+    const auto value = client.read_attribute_custom_node<int32_t>("the.answer");
+
+    if (not value.has_value()) {
+        std::cerr << "Failed to read the value" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "the value is: " << value.value() << std::endl;
+
+    return EXIT_SUCCESS;
 }
--- include/client.hpp
+++ include/client.hpp
@@ -2,7 +2,13 @@
 #define OPC_UA_DEMO_CLIENT_HPP
 
 #include <open62541/client.h>
+#include <open62541/client_highlevel.h>
+
+#include <expected>
+#include <string>
 #include <string_view>
+
+#include "variant.hpp"
 
 namespace demo {
     class Client {
@@ -13,9 +19,31 @@ namespace demo {
         [[nodiscard]] auto get_client() const -> UA_Client *;
         [[nodiscard]] auto connect(std::string_view url) const -> UA_StatusCode;
 
+        template<typename T>
+        [[nodiscard]] auto read_attribute_custom_node(std::string id)
+                -> std::expected<T, UA_StatusCode>;
+
     private:
         UA_Client *_client;
     };
+
+    template<typename T>
+    [[nodiscard]] auto Client::read_attribute_custom_node(std::string id)
+            -> std::expected<T, UA_StatusCode> {
+        auto variant = make_variant();
+        constexpr auto namespace_index = 1;
+        if (const auto status = UA_Client_readValueAttribute(
+                    _client, UA_NODEID_STRING(namespace_index, id.data()), &variant);
+            status != UA_STATUSCODE_GOOD or
+            not UA_Variant_hasScalarType(&variant, &UA_TYPES[UA_TYPES_INT32])) {
+            return std::unexpected(status);
+        }
+
+        const auto data = *static_cast<UA_Int32 *>(variant.data);
+        UA_Variant_clear(&variant);
+
+        return data;
+    }
 }// namespace demo
 
 #endif//OPC_UA_DEMO_CLIENT_HPP
