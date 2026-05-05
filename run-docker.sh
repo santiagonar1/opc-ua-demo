9f25c73d6d306b24883f1d53a99aa31bdd00f3a7
Santiago Narvaez <santiagonar1@gmail.com>
2026-05-05 18:28:00 +0200

    refactor: add docker file

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-DOCKER_IMAGE=santiagonar1/cpp-dev:latest
+DOCKER_IMAGE=opc-ua-demo
 FOLDER_NAME=code
 
+docker build -t ${DOCKER_IMAGE} .
 docker run --rm -it -v .:/${FOLDER_NAME} -w /${FOLDER_NAME} ${DOCKER_IMAGE} bash
